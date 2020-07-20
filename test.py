c6e39e71f5cbd3c683e6e4ad4b86ecc1a0b57a12
kaustubhgupta <kaustubhgupta1828@gmail.com>
2020-07-20 14:55:01 +0530

    some more commits:

---
+++ test.py
@@ -0,0 +1,17 @@
+# TODO: Some task for a project like a basic one shown here
+import os
+
+
+class Disp:
+    def __init__(self):
+        self.name = ''
+        self.roll_no = ''
+
+    def show(self, name,roll_no):
+        self.name = name
+        self.roll_no = roll_no
+        return self.name, self.roll_no
+
+
+a = Disp()
+print(a.show(name='Kaustubh', roll_no=12))
